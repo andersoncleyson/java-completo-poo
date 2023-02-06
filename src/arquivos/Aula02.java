d2d9d5587de98bde389c55db3eaddb713f3d9730
Anderson <andersoncleyson@protonmail.com>
2023-02-06 06:02:20 -0300

    Bloco try-with-resources

--- src/arquivos/Aula02.java
+++ src/arquivos/Aula02.java
@@ -7,14 +7,10 @@ import java.io.IOException;
 public class Aula02 {
     public static void main(String[] args) {
 
-        // FileReader e BufferedReader
-        String path = "/home/robot/codes/JAVA/java-completo-poo/src/arquivos/hello.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
+        // Bloco try-with-resources
+        String path = "src/arquivos/hello.txt";
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr);
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
 
             String line = br.readLine();
 
@@ -26,18 +22,6 @@ public class Aula02 {
         catch (IOException e){
             System.out.println("Error: " + e.getMessage());
         }
-        finally {
-            try {
-                if(br != null) {
-                    br.close();
-                }
-                if(fr != null){
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+
     }
 }
